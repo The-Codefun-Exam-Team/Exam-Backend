56f0e87d055f6913c02e0a7854070dbc25bb3754
Unknown15082 <trangiahuy15082006@gmail.com>
2022-09-08 13:02:37 +0700

    Temporary debug problem

--- general/ping.go
+++ general/ping.go
@@ -9,3 +9,7 @@ import (
 func Ping(c echo.Context) error {
 	return c.String(http.StatusOK, "pingmongus\n")
 }
+
+func TempDebug(c echo.Context) error {
+	return c.String(http.StatusOK, `{ "data":{ "problem":{ "code": "P001", "id": 23, "name": "P001" }, "language": "C++", "result": "AC", "score": 100, "code": "#include <iostream>\nusing namespace std;\n\nint main() {\n int x;\n cin >> x;\n cout << 2*x;\n}" } }`)
+} 
--- main.go
+++ main.go
@@ -1,42 +1,16 @@
 package main
 
 import (
-	"log"
-	"fmt"
-
 	"github.com/labstack/echo/v4"
 
-	"github.com/The-Codefun-Exam-Team/Exam-Backend/db"
 	"github.com/The-Codefun-Exam-Team/Exam-Backend/general"
 )
 
 func main(){
 	e := echo.New()
 
-	cf_db, err := db.New("unknown:password@tcp(localhost:3306)/codefun")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	rows, err := cf_db.Query("SELECT * FROM subs_code LIMIT 1")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	defer rows.Close()
-
-	for rows.Next() {
-		var rid int
-		var code string
-		var er string
-
-		if err := rows.Scan(&rid, &code, &er); err != nil {
-			log.Fatal(err)
-		}
-
-		fmt.Printf("rid: %v, code: %v, er: %v", rid, code, er)
-	}
-
 	e.GET("/ping", general.Ping)
+	e.GET("/debug_problem", general.TempDebug)
+
 	e.Start(":1700")
 }
