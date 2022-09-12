d47fe3cd0d9a8cfd716e897155629404576a26f6
Unknown15082 <trangiahuy15082006@gmail.com>
2022-09-12 14:08:23 +0700

    log

--- debug_submission/handler.go
+++ debug_submission/handler.go
@@ -3,6 +3,7 @@ package debugsubmission
 import (
 	"net/http"
 	"strconv"
+	"log"
 
 	"github.com/labstack/echo/v4"
 
@@ -10,10 +11,14 @@ import (
 )
 
 func (g *Group) SubmissionGet(c echo.Context) error {
+	log.Print("Getting ID")
+	
 	id, err := strconv.Atoi(c.Param("id"))
 	if err != nil {
 		return err
 	}
+
+	log.Print("Verifying")
 
 	u, err := models.Verify(c.Request().Header.Get("Authorization"))
 	if err != nil {
@@ -24,24 +29,34 @@ func (g *Group) SubmissionGet(c echo.Context) error {
 		return c.String(http.StatusForbidden, "Invalid token")
 	}
 
+	log.Print("Resolving queue")
+
 	err = models.ResolveQueue(g.db)
 	if err != nil {
 		return err
 	}
+
+	log.Print("Reading debug submission")
 
 	sub, err := models.ReadDebugSubmission(g.db, id)
 	if err != nil {
 		return err
 	}
 
+	log.Print("Checking owner")
+
 	if u.Data.Tid != sub.Tid {
-		return c.String(http.StatusForbidden, "Invalid token")
+		return c.String(http.StatusForbidden, "Not the owner")
 	}
+
+	log.Print("Reading JSON Submission")
 
 	jsub, err := models.ReadJSONDebugSubmission(g.db, id)
 	if err != nil {
 		return err
 	}
+
+	log.Print("Returning")
 
 	return c.JSON(http.StatusOK, &jsub)
 }
