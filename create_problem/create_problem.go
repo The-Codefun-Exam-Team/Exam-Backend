ddf36b9cedc240664e587e07afec6b01774c3824
Unknown15082 <trangiahuy15082006@gmail.com>
2023-07-05 21:57:21 +0700

    Create problem (WIP)

--- create_problem/create_problem.go
+++ create_problem/create_problem.go
@@ -1,18 +1,28 @@
 package create
 
 import (
+	"fmt"
 	"net/http"
+	"strconv"
 
 	"github.com/The-Codefun-Exam-Team/Exam-Backend/models"
 	"github.com/The-Codefun-Exam-Team/Exam-Backend/utility"
 	"github.com/labstack/echo/v4"
 )
 
+type CreateResult struct {
+	Status string `json:"status"`
+	Message string `json:"message"`
+	DPCode string `json:"code,omitempty"`
+}
+
 func (m *Module) CreateProblem(c echo.Context) (err error) {
 	// Verify the user first
 	user, err := utility.Verify(c, m.env)
-	if user == nil {
-		return err
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, models.Response{
+			Error: "An error has occured",
+		})
 	}
 
 	if user.Status != "Admin" {
@@ -21,7 +31,64 @@ func (m *Module) CreateProblem(c echo.Context) (err error) {
 		})
 	}
 
-	// TODO(unknown): Create the problem
+	id, err := strconv.Atoi(c.FormValue("id"))
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, models.Response{
+			Error: "Invalid ID",
+		})
+	}
+	m.env.Log.Infof("Creating problem for submission %v", id)
 
-	return nil
+	m.env.Log.Debugf("Checking for duplication")
+	
+	duplicated_code, err := checkDuplicated(m.env.DB, id)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, models.Response{
+			Error: "An error has occured",
+		})
+	}
+
+	if duplicated_code != "" {
+		m.env.Log.Infof("Problem already existed for ID %v (%v)", id, duplicated_code)
+		return c.JSON(http.StatusAccepted, models.Response{
+			Data: CreateResult{
+				Status: "DUPLICATED",
+				Message: fmt.Sprintf("Problem already existed (%v)", duplicated_code),
+			},
+		})
+	}
+
+	m.env.Log.Debugf("Getting problem code")
+
+	dpcode := c.FormValue("code")
+	if dpcode == "" {
+		m.env.Log.Debugf("Getting problem code suggestion")
+		dpcode, err = getSuggestedCode(m.env.DB)
+		if err != nil {
+			return c.JSON(http.StatusInternalServerError, models.Response{
+				Error: "An error has occured",
+			})
+		}
+		m.env.Log.Infof("Suggesting %v as problem code", dpcode)
+	}
+
+	m.env.Log.Debugf("Getting problem name")
+
+	name := c.FormValue("name")
+	if name == "" {
+		name = dpcode
+	}
+
+	// TODO: Check for FAILED
+
+	// TODO: Actually adding the problem
+
+	m.env.Log.Infof("Created problem %v", dpcode)
+	return c.JSON(http.StatusAccepted, models.Response{
+		Data: CreateResult{
+			Status: "OK",
+			Message: "Problem added successfully",
+			DPCode: dpcode,
+		},
+	})
 }
